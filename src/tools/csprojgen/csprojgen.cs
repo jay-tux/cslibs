6212dfd44215be0728cf0350550c4e80b33e30a0
jay-tux <sys.jonas@yahoo.com>
2020-09-24 15:33:53 +0200

    Finished csprojgen tool

--- src/tools/csprojgen/csprojgen.cs
+++ src/tools/csprojgen/csprojgen.cs
@@ -18,28 +18,40 @@ namespace Jay.CSLibs.Tools.CSProjGen
                 outfile = (args[0].EndsWith(".csproj") ? args[0] : (args[0] + ".csproj"));
                 shift = true;
             }
-            else
+            
+            for(int i = (shift ? 1 : 0); i < args.Length; i += 2)
             {
-                for(int i = (shift ? 1 : 0); i < args.Length; i += 2)
+                try
                 {
-                    try
-                    {
-                        switches[args[i].Remove(0, 1)] = switches[args[i + 1]];
-                    }
-                    catch(IndexOutOfRangeException)
-                    {
-                        Console.Error.WriteLine($"Switch {args[i]} ignored.");
-                    }
+                    switches[args[i].Remove(0, 1)] = args[i + 1];
+                    Console.WriteLine($"Added {args[i].Remove(0, 1)}");
+                }
+                catch(IndexOutOfRangeException)
+                {
+                    Console.Error.WriteLine($"Switch {args[i]} ignored.");
                 }
             }
-            Console.WriteLine($"Output File: {outfile}");
-            Console.WriteLine($"Switches: ");
-            foreach(var v in switches) { Console.WriteLine($"\t{v.Key} = {v.Value}"); }
+
+            if(switches.ContainsKey("out") && !(new List<string>() { "Library", "Exe", "Module", "WinExe" }).Contains(switches["out"]))
+            {
+                Console.Error.WriteLine("Unknown output type: " + switches["out"] + ". Using default 'Exe'.");
+                switches["out"] = "Exe";
+            }
+
+            File.WriteAllText(outfile, $"<Project Sdk=\"{ (switches.ContainsKey("sdk") ? switches["sdk"] : "Microsoft.Net.Sdk") }\">\n" +
+                $"\t<PropertyGroup>\n" +
+                $"\t\t<OutputType>{ (switches.ContainsKey("out") ? switches["out"] : "Exe") }</OutputType>\n" +
+                $"\t\t<TargetFramework>{ (switches.ContainsKey("frame") ? switches["frame"] : "netcoreapp3.1") }</TargetFramework>\n" +
+                $"\t\t<AssemblyName>{ (switches.ContainsKey("name") ? switches["name"] : Directory.GetCurrentDirectory().Split('/').Last()) }</AssemblyName>\n" +
+                "\t</PropertyGroup>\n</Project>");
         }
     }
 }
 
 /*
 csprojgen [outfile] [switches]
-
+    -out: output type, one of { Library, Exe, Module, WinExe } (default: Exe)
+    -frame: framework version, no checking done (default: netcoreapp3.1)
+    -name: assembly name, (default: directory name)
+    -sdk: sdk (default: Microsoft.Net.Sdk)
 */
